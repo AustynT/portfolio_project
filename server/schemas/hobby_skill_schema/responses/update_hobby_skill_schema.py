6dfb1994b86432d8d801f56c17e98c38722c7b54
AustynT <Austyntravis9@gmail.com>
2023-12-06 16:17:01 -0700

    Add hobby_skill_schema files

--- server/schemas/hobby_skill_schema/responses/update_hobby_skill_schema.py
+++ server/schemas/hobby_skill_schema/responses/update_hobby_skill_schema.py
@@ -5,10 +5,5 @@ class UpdateHobbySkillSchema(Schema):
     """
     Schema class for validating update type requests.
     """
-    id = fields.Integer(required=True)
-    name = fields.String
-    description = fields.String
-    user_id = fields.Integer(required=True)
-    type_id = fields.Integer(required=True)
-    title = fields.String(required=True)
-    description = fields.String(required=True)
+    hobby_id = fields.Integer(required=True)
+    skill_id = fields.Integer(required=True)
--- server/schemas/user_skill_schmea/responses/delete_hobby_skill_schema.py
+++ server/schemas/user_skill_schmea/responses/delete_hobby_skill_schema.py
@@ -5,5 +5,4 @@ class DeleteHobbySkillSchema(Schema):
     """
     Schema class for validating delete type requests.
     """
-    hobby_id = fields.Integer(required=True)
-    skill_id = fields.Integer(required=True)
+    id = fields.Int(required=True)
