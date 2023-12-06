ad2762e04dbc7f70d4ab1a99be816e0940180ee2
AustynT <Austyntravis9@gmail.com>
2023-12-06 00:14:48 -0700

    Refactor project_model.py to include project attributes and relationships

--- server/models/project_model.py
+++ server/models/project_model.py
@@ -1,7 +1,21 @@
 from server import db
 from .base_model import BaseModel
 
+
 class ProjectModel(BaseModel):
+    """
+    Represents a project in the system.
+
+    Attributes:
+        project_id (int): The unique identifier for the project.
+        user_id (int): The ID of the user who created the project.
+        type_id (int): The ID of the type of the project.
+        title (str): The title of the project.
+        description (str): The description of the project.
+        project_image (str): The image of the project.
+        created_at (datetime): The date and time the project was created.
+        project_skills (list): The skills associated with this project.
+    """
     __tablename__ = 'projects'
     project_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
