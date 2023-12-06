7e5147bcc552b28265d65b1f62dc4415ada74b82
AustynT <Austyntravis9@gmail.com>
2023-12-06 01:04:54 -0700

    Add project schema request and response classes
    This commit adds the project schema request and response classes for
    create, delete, read, and update operations. These classes are used for
    validating the request and response data.

---
+++ server/schemas/project_schema/responses/read_project_schema.py
@@ -0,0 +1,9 @@
+from marshmallow import Schema, fields
+
+
+class ReadProjectRequestSchema(Schema):
+    """
+    Schema class for validating read type requests.
+    """
+    project_id = fields.Int(required=True)
+    user_id = fields.Integer(required=True)
