b84bd0080b1c211e7a25fdd52d138176cbda04db
AustynT <Austyntravis9@gmail.com>
2023-12-11 11:42:03 -0700

    Refactor API route registration

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,14 +1,25 @@
-from flask import Flask
-from . import api_classes
-from .. import app
+from .auth_api import AuthApi
+from .user_api import UserApi
+from .user_info_api import UserInfoApi
+from .user_skill_api import UserSkillApi
+from .skill_api import SkillApi
+from .work_history import WorkHistoryApi
+from .type_api import TypeApi
+from .hobby_api import HobbyApi
+from .hobby_skill_api import HobbySkillApi
+from .project_api import ProjectApi
+from .project_skill_api import ProjectSkillApi
 
 
-def register_api_routes(app):
-    # Register API routes here using the app object
-    # For example:
-    app.add_url_rule(
-        '/api/users', view_func=api_classes.UsersAPI.as_view('users_api'))
-
-
-# Call the function with the app object
-register_api_routes(app)
+def register_routes(app):
+    app.add_route('/auth', AuthApi())
+    app.add_route('/user', UserApi())
+    app.add_route('/user_info', UserInfoApi())
+    app.add_route('/user_skill', UserSkillApi())
+    app.add_route('/skill', SkillApi())
+    app.add_route('/work_history', WorkHistoryApi())
+    app.add_route('/type', TypeApi())
+    app.add_route('/hobby', HobbyApi())
+    app.add_route('/hobby_skill', HobbySkillApi())
+    app.add_route('/project', ProjectApi())
+    app.add_route('/project_skill', ProjectSkillApi())
